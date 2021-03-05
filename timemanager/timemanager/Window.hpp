848027979ab589498f8d7882b5018450c4cdad1e
alekseevad <alkseev2001ad@gmail.com>
2021-03-05 17:28:03 +0300

    Entry point changed. Code refactored.

--- timemanager/timemanager/Window.hpp
+++ timemanager/timemanager/Window.hpp
@@ -7,15 +7,14 @@ class Window
 {
 public:
 	Window();
+
 	//Initialize the window
 	bool init();
 	bool broadcast();
+
 	//Release the window
 	bool release();
 	bool isRun();
-
-	//edit window
-	void OnSize(HWND hwnd, UINT flag, int width, int height);
 
 	//EVENTS
 	virtual void onCreate();
@@ -25,7 +24,7 @@ public:
 
 	~Window();
 protected:
-	HWND m_hwnd;
-	bool m_is_run;
+	HWND hwnd;
+	bool isRunning;
 };
 
--- timemanager/timemanager/main.cpp
+++ timemanager/timemanager/main.cpp
@@ -1,20 +1,52 @@
 
 
-#include "../Header/AppWindow.hpp"
+#include "Window.hpp"	
 
 
 
-int main()
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmdShow)
 {
-	AppWindow app;
-	if (app.init())
+	MSG msg{};
+	HWND hwnd{};
+	WNDCLASSEX wc{ sizeof(WNDCLASSEX) };
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = [](HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)->LRESULT
 	{
-		while (app.isRun())
+		switch (uMsg)
 		{
-			app.broadcast();
+			case WM_DESTROY:
+			{
+				PostQuitMessage(EXIT_SUCCESS);
+			}
+
+			return 0;
 		}
+
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	};
+	wc.lpszClassName = L"MyAppClass";
+	wc.lpszMenuName = nullptr;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+
+	if (!RegisterClassEx(&wc))
+		return EXIT_FAILURE;
+	if (hwnd = CreateWindow(wc.lpszClassName, L"Title!", WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, nullptr, nullptr, wc.hInstance, nullptr); hwnd == INVALID_HANDLE_VALUE)
+		return EXIT_FAILURE;
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 	}
 
+	return static_cast<int>(msg.wParam);
 
-	return 0;
 }
